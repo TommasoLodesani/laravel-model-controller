24b34ba42e4c996091dcc0dd7542ca7c7d57724a
Tommaso <tommaso.lodesani@gmail.com>
2022-07-14 16:22:34 +0200

    link db

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,11 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Movie;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index(){
         return view('home');
+    }
+
+    public function list(){
+       $movies =  Movie::all();
+       dd($movies);
+
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/','HomeController@index')->name('home');
+
+Route::get('/list','HomeController@list')->name('list');
